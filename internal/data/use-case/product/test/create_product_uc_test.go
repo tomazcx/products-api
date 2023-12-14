670643da1cc33097eb8859890d2a34362d71ca5e
tomazcx <tomazcx06@gmail.com>
2023-12-14 12:53:17 -0300

    feat: added docker and air auto reload

---
+++ internal/data/use-case/product/test/create_product_uc_test.go
@@ -0,0 +1,5 @@
+package test
+
+import "testing"
+
+func TestCreateProductUseCase(t *testing.T) {}
---
+++ internal/data/use-case/product/test/show_product_uc_test.go
@@ -0,0 +1,7 @@
+package test
+
+import "testing"
+
+func TestShowProductUseCase(t *testing.T) {
+
+}
